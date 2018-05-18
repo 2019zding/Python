0800647a70769824366c118a853551e5882c6310
Zara <2019zding@dwight.edu>
2018-05-17 20:31:48 -0400

    reverse array updated

--- reverseArray.py
+++ reverseArray.py
@@ -1,9 +1,8 @@
-def reverseArray(self):
-    n = len(self.items)
+def reverseArray(arr):
+    n = len(arr)
     for x in range(n):
-        for y in range(0,n-x-1):
-#reverse the inequality sign
-            if self.items[y] < self.items[y+1]:
-                #swap numbers
-                self.items[y], self.items[y+1] = self.items[y+1], self.items[y]
-    print(self.items)
+        for y in range(0, n-x-1):
+            #reverse the inequality sign so that it reverses array
+            if arr[y] < arr[y+1] :
+                arr[y], arr[y+1] = arr[y+1], arr[y]
+    return arr
